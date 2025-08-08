2fef6f566987ef0c42c627c85d8df0e8f5a1b928
Archibaldthe3rdjr <jackthefat404@gmail.com>
2025-08-08 21:53:45 +1000

    Added more to it such as slow text and it works

--- DND-Style-Game.py
+++ DND-Style-Game.py
@@ -1,11 +1,23 @@
 import time
-NameVERI = ("no")
-while NameVERI == ("no"):
+
+NameVERI = "no"
+
+while NameVERI == "no":
     Name = input("what is your name?")
     time.sleep(1)
     print(Name)
     NameVERI = input("Are you sure? (yes or no)")
-def Dot(
+
+def Dot():
     time.sleep(1)
     print(".")
-    )
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+
+
+Dot()
+print("morning "+Name+" its time for breakfast!")
+
