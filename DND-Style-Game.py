a845ab0137519743509bbdd777589faf6fa5e80a
Archibaldthe3rdjr <jackthefat404@gmail.com>
2025-08-11 15:43:19 +1000

    tMinecrafte

--- DND-Style-Game.py
+++ DND-Style-Game.py
@@ -22,10 +22,24 @@ while NameVERI == "no":
     print(Name)
     NameVERI = input("Are you sure? (yes or no)")
 
-
 Dot()
 slow_type("You awake to your alarm clock beeping", 0.10)
 time.sleep(.5)
+print("\n")
 slow_type("morning "+Name+" its time for breakfast!", 0.07)
 time.sleep(.5)
+print("\n")
 slow_type("You roll out of you bed to see your filthy room", 0.10)
+time.sleep(1)
+print("\n")
+slow_type("What do you wish to do?")
+Choice1 = input(slow_type("1.Leave Your Room\n 2.Clean Your Room\n 3.Call Out to Mum", 0.10))
+
+if Choice1 == "1":
+    print("You chose 1")
+
+if Choice1 == "2":
+    print("You chose 2")
+
+if Choice1 == "3":
+    print("You chose 3")
