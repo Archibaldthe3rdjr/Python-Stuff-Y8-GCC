133f373b9ef2c3dcca0755c3bc25941dbe5e4e54
Archibaldthe3rdjr <jackthefat404@gmail.com>
2025-08-22 06:34:33 +1000

    Just changed the new file up a bit

--- Rooms_Of_Deception.py
+++ Rooms_Of_Deception.py
@@ -1,6 +1,6 @@
 import time
 import os
-
+import pyfiglet
 
 def slow_type(text):
     for char in text:
@@ -18,14 +18,35 @@ while NameV != "yes":
     print("\n"+Name+"\n")
     NameV = input("Are You Sure?\n")
     NameV = NameV.lower()
+
+
+
+#start of game
+
 slow_type("You awake to your alarm clock\n")
 slow_type("\nMorning "+Name+" time for breakfast")
 slow_type("\nWhat do you wish to do?")
 slow_type("\n1.Exit Your Room\n2.Go Back to Sleep\n3.Clean Your Room")
 Choice1 = input()
 
+
 if Choice1 == "1":
     slow_type("You walked out of your room")
 
+
 if Choice1 == "2":
-    slow_type("")
+    slow_type("\nYou went back to bed\n")
+    slow_type("\nYou successfully lost the game\n")
+    print("\n"*10000)
+    print(pyfiglet.figlet_format("5"))
+    time.sleep(1)
+    print(pyfiglet.figlet_format("4"))
+    time.sleep(1)
+    print(pyfiglet.figlet_format("3"))
+    time.sleep(1)
+    print(pyfiglet.figlet_format("2"))
+    time.sleep(1)
+    print(pyfiglet.figlet_format("1"))
+    time.sleep(1)
+    print(pyfiglet.figlet_format("Good Bye "+(Name)))
+    time.sleep(1)
